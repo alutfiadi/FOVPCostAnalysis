d58dc4d021c40bac670797d567247823d1ac8781
alutfiadi <ahmadlutfiadi@gmail.com>
2020-07-06 02:49:02 +0000

    init

---
+++ webapp/Component.js
@@ -0,0 +1,13 @@
+sap.ui.define(["sap/ovp/app/Component"], function (Component) {
+	"use strict";
+	/**
+	 * @fileOverview Application component to display information on entities from the GWSAMPLE_BASIC
+	 *   OData service.
+	 * @version @version@
+	 */
+	return Component.extend("ZAL.CostAnalysisOVP3.Component", {
+		metadata: {
+			manifest: "json"
+		}
+	});
+});
